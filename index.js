c37ac287e689ba24a5ec4ef63ee768a4b04afff3
promisemagoga <tshegofatsopromise362@gmail.com>
2023-12-07 10:45:25 +0200

    7/12/2023

--- index.js
+++ index.js
@@ -22,18 +22,18 @@ app.use(express.urlencoded({ extended: true }))
 //const image = " https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
 const upload = multer({ storage: multer.memoryStorage() });
 //Add Employee
-app.post("/", upload.single('image') ,async (req, res) => {
+app.post("/", upload.single('image'), async (req, res) => {
     try {
-      
+
         const bucket = admin.storage().bucket();
         const file = bucket.file(req.file.originalname);
         const result = await file.createWriteStream().end(req.file.buffer);
         const downloadUrl = await file.getSignedUrl({
-          action: 'read',
-          expires: '03-17-2025'
+            action: 'read',
+            expires: '03-17-2025'
         });
-console.log(downloadUrl);
-        const imgJson ={
+        console.log(downloadUrl);
+        const imgJson = {
             imageUrl: downloadUrl[0]
         }
 
@@ -46,8 +46,8 @@ console.log(downloadUrl);
             employeeImg: downloadUrl[0]
         }
 
-        const response = await db.collection('employees').add(employeeJson).then(() =>{
-            res.redirect("home")
+        const response = await db.collection('employees').add(employeeJson).then(() => {
+            res.redirect("/")
         })
         res.send(response)
     } catch (error) {
@@ -76,26 +76,26 @@ app.put("/update/:id", async (req, res) => {
         employeePosition: req.body.employeePosition,
         employeePhoneNumber: req.body.employeePhoneNumber
     }
-    db.collection("employees").doc(id).update(updateData).then(() =>{
+    db.collection("employees").doc(id).update(updateData).then(() => {
         console.log("data is successfully updated");
-    }).catch((error) =>{
-         console.log(error);
-         res.send(error)
+    }).catch((error) => {
+        console.log(error);
+        res.send(error)
     })
 })
 
 
-app.get("/profile/:id", async(req,res) =>{
+app.get("/profile/:id", async (req, res) => {
     try {
-        const employeeRef = db.collection("employees").doc(req.params.id); 
+        const employeeRef = db.collection("employees").doc(req.params.id);
         const response = await employeeRef.get();
         if (!response.exists) {
             res.status(404).send("Employee not found");
             return;
         }
         const employeeData = response.data();
-        const employeeArray = [{...employeeData, id: response.id}];
-        res.render("profile", {responseArray: employeeArray})
+        const employeeArray = [{ ...employeeData, id: response.id }];
+        res.render("profile", { responseArray: employeeArray })
         console.log(employeeArray);
     } catch (error) {
         console.log(error);
@@ -103,16 +103,16 @@ app.get("/profile/:id", async(req,res) =>{
     }
 })
 
-app.get("/", async(req,res) =>{
+app.get("/", async (req, res) => {
     try {
-       const employeeRef = db.collection("employees") 
-       const response = await employeeRef.get();
-       let responseArray = [];
-       response.forEach((results) =>{
-        responseArray.push({...results.data(), id:results.id})
-       })
-       res.render("home", {responseArray})
-       console.log(responseArray);
+        const employeeRef = db.collection("employees")
+        const response = await employeeRef.get();
+        let responseArray = [];
+        response.forEach((results) => {
+            responseArray.push({ ...results.data(), id: results.id })
+        })
+        res.render("home", { responseArray })
+        console.log(responseArray);
     } catch (error) {
         console.log(error);
         res.send(error)
